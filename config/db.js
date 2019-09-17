d2ab94f96efc3e33464d3f052cbd75b73499910f
mpmcauliffe <mcauliffe99@gmail.com>
2019-09-17 02:05:57 -0600

    connected to Mongo

---
+++ config/db.js
@@ -0,0 +1,27 @@
+// SEE README.md
+
+const mongoose      = require('mongoose')
+const config        = require('config')
+
+const db            = config.get('MONGO_URI')
+
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(db, 
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useFindAndModify: false,   
+            }
+        )
+        console.log('MongoDB connected :)')
+    } catch (err) {
+        console.error(err.message)
+        process.exit(1)
+    }
+}
+
+
+module.exports = connectDB
