ce0eeca24915651687863b35d8813956b4ad197e
mpmcauliffe <mcauliffe99@gmail.com>
2019-10-16 22:38:10 -0600

    attach front end for first deployment test

--- server.js
+++ server.js
@@ -28,20 +28,14 @@ app.use(
 app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
 
-//////////////////////////////////////////////////////////////////////
-
-// app.get('/', (req, res) => {
-//     res.json({ msg: 'welcome to the contacts app'})
-// })
-
-
 // serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     // set static folder
-//     app.use(express.static('client/build'))
+if (process.env.NODE_ENV === 'production') {
+    //app.use(express.static('client/build'))
 
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-// }
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 
 app.listen(PORT, () => console.log(`Server up on ${PORT}`))
+
+//heroku config:set key=value
