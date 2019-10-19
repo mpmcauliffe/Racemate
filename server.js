d99f7acfe9b1ab0a75d3a71bf9c68826c030986a
mpmcauliffe <mcauliffe99@gmail.com>
2019-10-18 22:26:44 -0600

    update CRUD functions

--- server.js
+++ server.js
@@ -29,13 +29,11 @@ app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
 
 // serve static assets in production
-//if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+// app.use(express.static('client/build'))
+// app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-//}
 
 
 app.listen(PORT, () => console.log(`Server up on ${PORT}`))
 
-//heroku config:set key=value
+// heroku config:set key=value
