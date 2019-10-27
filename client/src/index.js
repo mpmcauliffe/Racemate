8a2348d62ad451d0da6190f2652d0e3b7b6a4e18
mpmcauliffe <mcauliffe99@gmail.com>
2019-10-26 22:44:06 -0600

    SCSS connection

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import 'normalize.css/normalize.css'
+import './scss/index.scss'
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
