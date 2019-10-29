aed94d62138091b9e1bb4a55ee51051e9876d1ce
mpmcauliffe <mcauliffe99@gmail.com>
2019-10-29 02:08:23 -0600

    structure update

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,14 @@
-import React from 'react'
-import './App.css'
+import React, { Fragment, } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Menu } from './components'
 
 
 const App = () => {
     return (
-        <div className="App">
-            <div className="navigation">
-                <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-
-                <label htmlFor="navi-toggle" className="navigation__button">
-                    <span className="navigation__icon">&nbsp;</span>
-                </label>
-
-                <div className="navigation__background">&nbsp;</div>
-
-                <nav className="navigation__nav">
-                    <ul className="navigation__list">
-                        <li className="navigation__item">
-                            <a href="#" className="navigation__link">Routine</a>
-                        </li>
-                        <li className="navigation__item">
-                            <a href="#" className="navigation__link">Workouts</a>
-                        </li>
-                        <li className="navigation__item">
-                            <a href="#" className="navigation__link">Exercises</a>
-                        </li>
-                        <li className="navigation__item">
-                            <a href="#" className="navigation__link">Logout</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
+        <Fragment>
+            <Menu />
+            <Switch></Switch>
+        </Fragment>
     )
 }
 
