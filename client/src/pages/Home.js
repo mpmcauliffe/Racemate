837a8c0fc86d3acc77fe1ca742872c966ccb3216
mpmcauliffe <mcauliffe99@gmail.com>
2020-01-14 02:06:25 -0700

    nothing works correctly

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { OpSwitchExperimental, } from '../components'
 
 import { GET_EXERCISES, } from '../graphql'
 
 
 export const Home = () => {
+
+    const [userSelection, setUserSelection] = useState('')
     const optButtons = ['Exercises', 'Workouts', 'Routine']
+
+    const handleToggle = buttonName => {
+        setUserSelection(buttonName)
+        console.log(userSelection)
+    }
 
     return (
         <div>
-            <OpSwitchExperimental optButtons={optButtons} />
+            <OpSwitchExperimental 
+                optButtons={optButtons}
+                handleToggle={handleToggle} />
         </div>
     )
 }
