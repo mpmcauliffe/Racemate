0f5948e82c77c50fc7511b93d8620e9f7d64cddb
mpmcauliffe <mcauliffe99@gmail.com>
2020-02-23 23:01:50 -0700

    test graphQL inputs

--- client/src/components/input/stopwatch/StopwatchComp.js
+++ client/src/components/input/stopwatch/StopwatchComp.js
@@ -31,6 +31,6 @@ export const LapContainer = styled.div`
 export const LapText = styled.p`
     font-size: 3rem;
     text-align: center;
-    line-spacing: 1rem;
+    line-spacing: 7rem;
     &&& { color: ${props => props.theme.secondaryDark}; }
 `
