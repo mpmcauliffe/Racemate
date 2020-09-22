35d4b4bcbe53b88f85dbde4d4f0274bacf4703d8
mpmcauliffe <mcauliffe99@gmail.com>
2020-09-21 20:04:56 -0600

    finally? MAYBE? the style in the card could be done?

--- client/src/components/GridStack/CardComp.js
+++ client/src/components/GridStack/CardComp.js
@@ -21,17 +21,23 @@ export const CardContainer = styled.div`
     background: ${props =>  props.theme.colorPrimary};
 
     div:first-of-type {
-        margin-top: 0rem;
+        margin-top: 2rem;
         align-self: flex-end; 
     }
     div button { 
-        height: 4rem;
+        /* height: 4rem; */
         text-align: right;
     }
-    p { margin-top: -3.5rem; }
+    p { 
+        width: 4.3rem;
+        margin: -3.5rem 0 0 0; 
+    }
 
     
-    @media (min-width: 481px) { p { font-size: 1.7rem; } }
+    @media (min-width: 481px) { 
+        div:first-of-type { margin-right: 1.5rem; }
+        p { font-size: 1.7rem; } 
+    }
     /* @media (min-width: 769px) { p { margin-top: 7%; } }
     @media (min-width: 1025px) { p { margin-top: 5%; } }
     @media (min-width: 1601px) { p { margin-top: 4%; } } */
@@ -70,6 +76,7 @@ export const IconButton = styled.button`
     /* grid-row-start: 1;
     grid-row-end: 3; */
 
+    height: 4rem;
     width: 13rem;
     color: ${props => props.theme.midLtGray};
     background: transparent;
